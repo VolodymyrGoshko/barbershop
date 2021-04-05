5e5c33e40f90de4868a0d294e379fff5ea7df552
VolodymyrGoshko <goshkovova771@gmail.com>
2021-04-05 19:18:58 +0300

    commit

--- app.js
+++ app.js
@@ -6,3 +6,45 @@ navToggle.addEventListener("click", () => {
     nav.classList.toggle('open')
     headerInner.classList.toggle('opened')
 })
+
+function createModal() {
+    const header = document.querySelector('.header')
+    header.insertAdjacentHTML('afterend', `
+        <div class="modal" data-general="true">
+            <div class="modal-content" >
+                <div class="modal-title" >Записатись</div>
+                <form class="form" >
+                    <input type="text" class="input-name"  placeholder="Your Name"/>
+                    <input type="email" class="input-email"  placeholder="Your email or phone"/>
+                    <input type="datetime-local" class="date"/>
+                    <textarea class="message" placeholder="Your wishes"></textarea>
+                </form>                
+                
+                <button class="modal-btn" type="submit">Надіслати</button>
+            </div>
+        </div>
+    `)
+
+    const dataLink = document.querySelectorAll('[data-modal]')
+    const modal = document.querySelector('.modal')
+    const modalContent = document.querySelector('.modal-content')
+    const modalBtn = document.querySelector('.modal-btn')
+
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'none'
+    })
+
+    dataLink.forEach(link => {
+        link.addEventListener('click', () => {
+            // modalContent.classList.toggle('open')
+            modal.style.display = 'block'
+        })
+    })
+
+    modal.addEventListener('click', event => {
+        if (event.target.dataset.general) {
+            modal.style.display = 'none'
+        }
+    })
+}
+createModal()
